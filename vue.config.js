c2e9d096cb17bb0ecfaeb3033d6355d66268f0da
Marcilio Cobel do Nascimento Junior <marcilio.nascimento@compasso.com.br>
2021-07-02 15:40:13 -0300

    removendo config external

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     if (process.env.NODE_ENV !== 'production') {
       config.output.filename(`js/[name].js`)
     }
-    config.externals(['vue', 'vue-router'])
+    //config.externals(['vue', 'vue-router'])
   },
   filenameHashing: false
 }
